422683ac954b8489421d7535bd3050f8a7161e02
Daniel Zacarias <daniel@DESKTOP-68C20A1>
2024-06-06 17:48:34 -0300

    Add CustomerChangeAddressEvent on LogWhenCustomerChangeHandler generics.

--- src/domain/customer/event/handler/log-when-customer-change-address.handle.ts
+++ src/domain/customer/event/handler/log-when-customer-change-address.handle.ts
@@ -1,9 +1,9 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import Customer from "../../entity/customer";
-import CustomerCreatedEvent from "../customer-created.event";
+import CustomerChangeAddressEvent from "../customer-change-address.event";
 
-export default class LogWhenCustomerChangeHandler implements EventHandlerInterface<CustomerCreatedEvent> {
-    handle(event: CustomerCreatedEvent): void {
+export default class LogWhenCustomerChangeHandler implements EventHandlerInterface<CustomerChangeAddressEvent> {
+    handle(event: CustomerChangeAddressEvent): void {
         const customer = event.eventData as Customer;
         console.log(`Endereço do cliente: ${customer.id}, ${customer.name} alterado para: ${customer.Address.street}, ${customer.Address.city}, ${customer.Address.number}`)
     }
